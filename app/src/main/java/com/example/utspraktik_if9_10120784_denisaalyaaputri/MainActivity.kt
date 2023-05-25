a7c20ef884bc0549cda04ed3fe0818c55ecf256b
Denisaputri09 <denisaputri789@gmail.com>
2023-05-25 09:51:10 +0700

    UTSPraktik

---
+++ app/src/main/java/com/example/utspraktik_if9_10120784_denisaalyaaputri/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if9_10120784_denisaalyaaputri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
